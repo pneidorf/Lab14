df0395e7c6cc59d338ecebd4e389e8129459b016
Pavel Neidorf <pnejdorf@mail.ru>
2025-03-25 11:10:15 +0700

    Add local storage

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ import "./download.js";
 
 document.querySelector("#app").innerHTML = `
   <div class="resume">
-    <h1 contenteditable="true" class="editable">Ваше Имя</h1>
-    <p contenteditable="true" class="editable">Ваша Профессия</p>
-    <section contenteditable="true" class="editable">
+    <h1 contenteditable="true" class="editable" id="name">Ваше Имя</h1>
+    <p contenteditable="true" class="editable" id="profession">Ваша Профессия</p>
+    <section contenteditable="true" class="editable" id="about">
       <h2>Обо мне</h2>
       <p>Описание о себе...</p>
     </section>
-    <section contenteditable="true" class="editable">
+    <section contenteditable="true" class="editable" id="experience">
       <h2>Опыт</h2>
       <p>Детали о вашем опыте...</p>
     </section>
-    <section contenteditable="true" class="editable">
+    <section contenteditable="true" class="editable" id="education">
       <h2>Образование</h2>
       <p>Детали о вашем образовании...</p>
     </section>
@@ -23,4 +23,21 @@ document.querySelector("#app").innerHTML = `
 
 document.getElementById("download-btn").addEventListener("click", () => {
   window.print();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const editableElements = document.querySelectorAll(".editable");
+
+  editableElements.forEach((element) => {
+    const savedContent = localStorage.getItem(`content_${element.id}`);
+    if (savedContent) {
+      element.innerHTML = savedContent;
+    }
+  });
+
+  editableElements.forEach((element) => {
+    element.addEventListener("input", () => {
+      localStorage.setItem(`content_${element.id}`, element.innerHTML);
+    });
+  });
 });
